69b8fb801d7d3e003652eb7b13fa1ff39febd1e2
eswariah_bhun <eswariah_bhun@gap.com>
2015-08-30 18:08:53 +0530

    mqueue programs

---
+++ src/main/java/com/dev/mqueue/MyMQSender.java
@@ -0,0 +1,143 @@
+package com.dev.mqueue;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import com.ibm.jms.JMSTextMessage;
+import com.ibm.mq.jms.JMSC;
+import com.ibm.mq.jms.MQQueue;
+import com.ibm.mq.jms.MQQueueConnection;
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.mq.jms.MQQueueSender;
+import com.ibm.mq.jms.MQQueueSession;
+
+public class MyMQSender {
+ /**
+  * Main method
+  * 
+  * @param args
+  */
+ public static void main(String[] args) {
+  try {
+			for (int i = 1; i <= 1; i++) {
+				sendMsg(i);
+	  }
+  } catch (JMSException jmsex) {
+   System.out.println(jmsex);
+   System.out.println("\\nFAILURE\\n");
+  } catch (Exception ex) {
+   System.out.println(ex);
+   System.out.println("\\nFAILURE\\n");
+  }
+ }
+
+private static void sendMsg(int i) throws JMSException {
+	MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
+	
+	   // Config
+	  
+	   
+	   
+	   cf.setHostName("dauschaap31.gap.dev");
+	   cf.setPort(1414);
+	   cf.setQueueManager("ENT01D");
+	   cf.setChannel("ENT01D.SVRCONN");
+	   
+	 
+	
+	   
+	   cf.setTransportType(JMSC.MQJMS_TP_CLIENT_MQ_TCPIP);
+	   MQQueueConnection connection = (MQQueueConnection) cf
+	     .createQueueConnection();
+	   MQQueueSession session = (MQQueueSession) connection
+	     .createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+	   MQQueue queue = (MQQueue) session
+			   //QC.FRN.LOGISTICS.DOCREADY.INPUT
+	     .createQueue("queue:///QC.FRN.LOGISTICS.OBASN.INPUT");
+	   MQQueueSender sender = (MQQueueSender) session.createSender(queue);
+	   // MQQueueReceiver receiver = (MQQueueReceiver)
+	   // session.createReceiver(queue);
+	
+	   // long uniqueNumber = System.currentTimeMillis() % 1000;
+	   String inputXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><dc:EBO xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://service.gap.com/schemas/DCOutboundASN.xsd\" xmlns:dc=\"http://service.gap.com/schemas/DC\">"
+			   + "<esb:ESBHeader xmlns:esb=\"http://service.gap.com/schemas/ESBHeader\">" 
+			   + "<esb:VersionNbr>1.0</esb:VersionNbr>"
+			   + "<esb:BusinessEvent>"
+	   + "<esb:TransactionTypeCode>DC_DR_OBASN</esb:TransactionTypeCode> " 
+	   +  "<esb:Timestamp>2014-01-22T12:40:08.636-08:00</esb:Timestamp>"
+	   + "<esb:EventID>OBASN_00000000000000000491</esb:EventID>"
+	   + "<esb:TotalRecordCount>1</esb:TotalRecordCount>"
+	   + "</esb:BusinessEvent>"
+	   + "<esb:SourceSystem>"
+	   + "<esb:DCID>27</esb:DCID>"
+	   + "<esb:ApplicationName>RGTI</esb:ApplicationName>"
+	   + "</esb:SourceSystem>"
+	   + "<esb:RoutingInformation>"
+	   + "<esb:Type>"
+	   + "<esb:RoutingTypeName>TRANSACTION</esb:RoutingTypeName>"
+	   + "<esb:Label>"
+	   + "<esb:LabelName>TRANSACTION_TYPE_CODE</esb:LabelName>"
+	   + "<esb:LabelValueText>DC_DR_OBASN</esb:LabelValueText>"
+	   + "</esb:Label>"
+	   + "</esb:Type>"
+	   + "</esb:RoutingInformation>"
+	   + "<esb:DatasetInfo>"
+	   + "<esb:TotalDatasetCount>1</esb:TotalDatasetCount>"
+	   + "<esb:DatasetNbr>1</esb:DatasetNbr>"
+	   + "<esb:DatasetSizeQty>1</esb:DatasetSizeQty>"
+	   + "<esb:KeyField1>RUG0000162</esb:KeyField1>"
+	   + "</esb:DatasetInfo>"
+	   + "</esb:ESBHeader>"
+	   + "<dc:EBOPayload>"
+	   + "<dc:OutboundASN>"
+	   + "<dc:TransactionType>DR</dc:TransactionType>"
+	   + "<dc:Corp>0001</dc:Corp>"
+	   + "<dc:MasterBOL>RUG0000162</dc:MasterBOL>"
+	   + "<dc:OriginatingDC>00027</dc:OriginatingDC>"
+	   + "<dc:ShipDate/>"
+	   + "<dc:PoolerLocationID/>"
+	   + "<dc:PoolerLabelID/>"
+	   + "<dc:SCAC/>"
+	   + "<dc:MemoBOL/>"
+	   + "<dc:TargetDate/>"
+	   + "<dc:TargetTime/>"
+	   + "<dc:Store>04321</dc:Store>"
+	   + "<dc:InStoreDate>20140122</dc:InStoreDate>"
+	   + "<dc:CartonID>00004200270396624146</dc:CartonID>"
+	   + "<dc:CartonContents>CB</dc:CartonContents>"
+	   + "<dc:Weight>00068</dc:Weight>"
+	   + "<dc:Volume>00010</dc:Volume>"
+	   + "<dc:ItemID>301471936</dc:ItemID>"
+	   + "<dc:PrepackFlag>0</dc:PrepackFlag>"
+	   + "<dc:Units>0000200</dc:Units>"
+	   + "<dc:ReferenceNumber>6000001525</dc:ReferenceNumber>"
+	   + "<dc:MerchandiseIndicator>MERCHANDISE</dc:MerchandiseIndicator>"
+	   + "<dc:PalletName/>"
+	   + "<dc:TMLoadID/>"
+	   + "<dc:VendorLocationCode/>"
+	   + "<dc:PO/>"
+	   + "<dc:MasterBOLTotalLines>2</dc:MasterBOLTotalLines>"
+	   + "<dc:MasterBOLLineNumber>"+i+"</dc:MasterBOLLineNumber>" 
+	   + "</dc:OutboundASN>"
+	   + "</dc:EBOPayload>"
+	   + "</dc:EBO>";  
+	   JMSTextMessage message = (JMSTextMessage) session
+	     .createTextMessage(inputXML);
+	   message.setJMSCorrelationID("1145611");
+	   // Start the connection
+	   connection.start();
+	   sender.send(message);
+	   System.out.println("Sent message:\\n" + message);
+	
+	   // JMSMessage receivedMessage = (JMSMessage)
+	   // receiver.receive(10000);
+	   // System.out.println("\\nReceived message:\\n" + receivedMessage);
+	
+	   sender.close();
+	   // receiver.close();
+	   session.close();
+	   connection.close();
+	
+	   System.out.println(i +"  \\nSUCCESS\\n ");
+}
+}
---
+++ src/main/java/com/dev/mqueue/QueueManager_OBASN.java
@@ -0,0 +1,48 @@
+package com.dev.mqueue;
+
+import com.ibm.mq.MQC;
+import com.ibm.mq.MQEnvironment;
+import com.ibm.mq.MQException;
+import com.ibm.mq.MQQueue;
+import com.ibm.mq.MQQueueManager;
+
+public class QueueManager_OBASN {
+
+    private final String host;
+    private final int port;
+    private final String channel;
+    private final String manager;
+    private final MQQueueManager qmgr;
+
+    public QueueManager_OBASN(String host, int port, String channel, String manager) throws MQException {
+        this.host = host;
+        this.port = port;
+        this.channel = channel;
+        this.manager = manager;
+        this.qmgr = createQueueManager();
+    }
+
+    public int depthOf(String queueName) throws MQException {
+        MQQueue queue = qmgr.accessQueue(queueName, MQC.MQOO_INQUIRE | MQC.MQOO_INPUT_AS_Q_DEF, null, null, null);
+        return queue.getCurrentDepth();
+    }
+
+    private MQQueueManager createQueueManager() throws MQException {
+        MQEnvironment.channel = channel;
+        MQEnvironment.port = port;
+        MQEnvironment.hostname = host;
+        MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
+        return new MQQueueManager(manager);
+    }
+    
+	public static void main(String[] args) throws MQException {
+//		QueueManager_OBASN manager = new QueueManager_OBASN("paigpa01.gap.com", 1414, "ENT01P.SVRCONN", "ENT01P");
+		QueueManager_OBASN manager = new QueueManager_OBASN("dauschaap31.gap.dev", 1414, "ENT01D.SVRCONN", "ENT01D");
+		System.out.println("Current queue depth: "+manager.depthOf("QC.FRN.LOGISTICS.OBASN.INPUT"));
+//		QC.FRN.LOGISTICS.OBASN.INPUT
+	}
+
+
+
+
+}
